fa8e22c20265dfb6b8a332822ed8134a5e0bdaeb
Charnock <charnockz@yahoo.com>
2014-11-09 21:59:10 -0500

    Update BoundingBoxManager.cpp

--- 0_TestBed/BoundingBoxManager.cpp
+++ 0_TestBed/BoundingBoxManager.cpp
@@ -21,7 +21,7 @@ void BoundingBoxManager::ReleaseInstance()
 void BoundingBoxManager::Init(void)
 {
 	m_pModelMngr = ModelManagerClass::GetInstance();
-	m_vCollidingNames.clear();
+	m_vAACollidingNames.clear();
 	m_nBoxes = 0;
 }
 void BoundingBoxManager::Release(void)
@@ -37,38 +37,74 @@ BoundingBoxManager::~BoundingBoxManager(){Release();};
 //Accessors
 int BoundingBoxManager::GetNumberOfBoxes(void){ return m_nBoxes; }
 //--- Non Standard Singleton Methods
-void BoundingBoxManager::SetVisible(bool a_bVisible, String a_sInstance)
+void BoundingBoxManager::SetAAVisible(bool a_bVisible, String a_sInstance)
 {
 	if(a_sInstance == "ALL")
 	{
 		int nBoxes = GetNumberOfBoxes();
 		for(int nBox = 0; nBox < nBoxes; nBox++)
 		{
-			m_vBoundingBox[nBox]->SetVisible(a_bVisible);
+			m_vBoundingBox[nBox]->SetAAVisible(a_bVisible);
 		}
 	}
 	else
 	{
 		int nBox = m_pModelMngr->IdentifyInstance(a_sInstance);
 		if(nBox < 0 || nBox < m_nBoxes)
-			m_vBoundingBox[nBox]->SetVisible(a_bVisible);
+			m_vBoundingBox[nBox]->SetAAVisible(a_bVisible);
 	}
 }
-void BoundingBoxManager::SetColor(vector3 a_v3Color, String a_sInstance)
+
+void BoundingBoxManager::SetOVisible(bool a_bVisible, String a_sInstance)
 {
 	if(a_sInstance == "ALL")
 	{
 		int nBoxes = GetNumberOfBoxes();
 		for(int nBox = 0; nBox < nBoxes; nBox++)
 		{
-			m_vBoundingBox[nBox]->SetColor(a_v3Color);
+			m_vBoundingBox[nBox]->SetOVisible(a_bVisible);
 		}
 	}
 	else
 	{
 		int nBox = m_pModelMngr->IdentifyInstance(a_sInstance);
 		if(nBox < 0 || nBox < m_nBoxes)
-			m_vBoundingBox[nBox]->SetColor(a_v3Color);
+			m_vBoundingBox[nBox]->SetOVisible(a_bVisible);
+	}
+}
+void BoundingBoxManager::SetAAColor(vector3 a_v3Color, String a_sInstance)
+{
+	if(a_sInstance == "ALL")
+	{
+		int nBoxes = GetNumberOfBoxes();
+		for(int nBox = 0; nBox < nBoxes; nBox++)
+		{
+			m_vBoundingBox[nBox]->SetAAColor(a_v3Color);
+		}
+	}
+	else
+	{
+		int nBox = m_pModelMngr->IdentifyInstance(a_sInstance);
+		if(nBox < 0 || nBox < m_nBoxes)
+			m_vBoundingBox[nBox]->SetAAColor(a_v3Color);
+	}
+}
+
+void BoundingBoxManager::SetOColor(vector3 a_v3Color, String a_sInstance)
+{
+	if(a_sInstance == "ALL")
+	{
+		int nBoxes = GetNumberOfBoxes();
+		for(int nBox = 0; nBox < nBoxes; nBox++)
+		{
+			m_vBoundingBox[nBox]->SetOColor(a_v3Color);
+		}
+	}
+	else
+	{
+		int nBox = m_pModelMngr->IdentifyInstance(a_sInstance);
+		if(nBox < 0 || nBox < m_nBoxes)
+			m_vBoundingBox[nBox]->SetOColor(a_v3Color);
 	}
 }
 void BoundingBoxManager::SetModelMatrix(matrix4 a_mModelMatrix, String a_sInstance)
@@ -88,7 +124,8 @@ void BoundingBoxManager::SetModelMatrix(matrix4 a_mModelMatrix, String a_sInstan
 			m_vBoundingBox[nBox]->SetModelMatrix(a_mModelMatrix);
 	}
 }
-void BoundingBoxManager::Render(String a_sInstance)
+
+void BoundingBoxManager::RenderAA(String a_sInstance)
 {
 	
 	if(a_sInstance == "ALL")
@@ -96,20 +133,40 @@ void BoundingBoxManager::Render(String a_sInstance)
 		int nBoxes = GetNumberOfBoxes();
 		for(int nBox = 0; nBox < nBoxes; nBox++)
 		{
-			m_vBoundingBox[nBox]->Render(MEDEFAULT);
+			m_vBoundingBox[nBox]->RenderAA(MEDEFAULT);
 		}
 	}
 	else
 	{
 		int nBox = m_pModelMngr->IdentifyInstance(a_sInstance);
 		if(nBox < 0 || nBox < m_nBoxes)
-			m_vBoundingBox[nBox]->Render(MEDEFAULT);
+			m_vBoundingBox[nBox]->RenderAA(MEDEFAULT);
+	}
+}
+
+void BoundingBoxManager::RenderO(String a_sInstance)
+{
+	
+	if(a_sInstance == "ALL")
+	{
+		int nBoxes = GetNumberOfBoxes();
+		for(int nBox = 0; nBox < nBoxes; nBox++)
+		{
+			m_vBoundingBox[nBox]->RenderO(MEDEFAULT);
+		}
+	}
+	else
+	{
+		int nBox = m_pModelMngr->IdentifyInstance(a_sInstance);
+		if(nBox < 0 || nBox < m_nBoxes)
+			m_vBoundingBox[nBox]->RenderO(MEDEFAULT);
 	}
 }
 void BoundingBoxManager::AddBox(String a_sInstanceName)
 {
 	BoundingBoxClass* oBox = new BoundingBoxClass(a_sInstanceName);
 	m_vBoundingBox.push_back(oBox);
+	oBox->SetModelMatrix(m_pModelMngr->GetModelMatrix(a_sInstanceName));
 	m_nBoxes ++;
 }
 void BoundingBoxManager::RemoveBox(String a_sInstanceName)
@@ -144,68 +201,173 @@ void BoundingBoxManager::RemoveBox(String a_sInstanceName)
 		}
 	}
 	m_vBoundingBox = vTemp;
-	m_nBoxes++;
+	m_nBoxes--;
 }
 void BoundingBoxManager::Update(void)
 {
-	m_vCollidingNames.clear();
+	m_vAACollidingNames.clear();
+	m_vOCollidingNames.clear();
 	for(int nBox = 0; nBox < m_nBoxes; nBox++)
 	{
-		m_vBoundingBox[nBox]->SetColor(MEWHITE);
+		m_vBoundingBox[nBox]->SetAAColor(MEYELLOW);
+		m_vBoundingBox[nBox]->SetOColor(MEBLUE);
 	}
 	CollisionCheck();
+	SATCollisionCheck();
 	CollisionResponse();
 }
 void BoundingBoxManager::CollisionCheck(void)
 {
-	//for(int nBox2 = 0; nBox2 < m_nBoxes; nBox2++)
-	//{
-	//	for(int nBox1 = 0; nBox1 < m_nBoxes; nBox1++)
-	//	{
-	//		if(nBox1 != nBox2)
-	//		{
-	//			//Get Radius of Box1
-	//			float fRadius1 = m_vBoundingBox[nBox1]->GetRadius();
-	//			
-	//			//Get Radius of Box2
-	//			float fRadius2 = m_vBoundingBox[nBox2]->GetRadius();
-	//			
-	//			//Get origin of Box1
-	//			matrix4 mMatrix1 = m_vBoundingBox[nBox1]->GetModelMatrix();
-	//			vector3 vCentroid1 = m_vBoundingBox[nBox1]->GetCentroid();
-	//			vector3 fOrigin1 = static_cast<vector3>(glm::translate(mMatrix1, vCentroid1) * vector4(0.0f, 0.0f, 0.0f, 1.0f));
-	//			
-	//			//Get origin of Box2
-	//			matrix4 mMatrix2 = m_vBoundingBox[nBox2]->GetModelMatrix();
-	//			vector3 vCentroid2 = m_vBoundingBox[nBox2]->GetCentroid();
-	//			vector3 fOrigin2 = static_cast<vector3>(glm::translate(mMatrix2, vCentroid2) * vector4(0.0f, 0.0f, 0.0f, 1.0f));
-	//			
-	//			float fDistance = glm::distance(fOrigin1,fOrigin2);
-	//			float fRadiusSum = fRadius1 + fRadius2;
-	//			if(fDistance < fRadiusSum)
-	//			{
-	//				m_vCollidingNames.push_back(m_vBoundingBox[nBox1]->GetInstanceName());
-	//				m_vCollidingNames.push_back(m_vBoundingBox[nBox2]->GetInstanceName());
-	//			}
-	//		}
-	//	}
-	//}
+	for(int nBox2 = 0; nBox2 < m_nBoxes; nBox2++)
+	{
+		for(int nBox1 = 0; nBox1 < m_nBoxes; nBox1++)
+		{
+			if(nBox1 != nBox2)
+			{
+				vector3 maximum1 = m_vBoundingBox[nBox1]->GetAAMaximum();
+				vector3 minimum1 = m_vBoundingBox[nBox1]->GetAAMinimum();
+				vector3 maximum2 = m_vBoundingBox[nBox2]->GetAAMaximum();
+				vector3 minimum2 = m_vBoundingBox[nBox2]->GetAAMinimum();
+
+				if(!(maximum1.x < minimum2.x || minimum1.x > maximum2.x) &&
+					!(maximum1.y < minimum2.y || minimum1.y > maximum2.y) &&
+					!(maximum1.z < minimum2.z || minimum1.z > maximum2.z))
+				{
+					m_vAACollidingNames.push_back(m_vBoundingBox[nBox1]->GetInstanceName());
+					m_vAACollidingNames.push_back(m_vBoundingBox[nBox2]->GetInstanceName());
+				}
+			}
+		}
+	}
 }
-bool BoundingBoxManager::CheckForNameInList(String a_sName)
+bool BoundingBoxManager::CheckForNameInAAList(String a_sName)
 {
-	int nNames = static_cast<int>(m_vCollidingNames.size());
+	int nNames = static_cast<int>(m_vAACollidingNames.size());
 	for(int nName = 0; nName < nNames; nName++)
 	{
-		if(m_vCollidingNames[nName] == a_sName)
+		if(m_vAACollidingNames[nName] == a_sName)
 			return true;
 	}
 	return false;
 }
+
+bool BoundingBoxManager::CheckForNameInOList(String a_sName)
+{
+	int nNames = static_cast<int>(m_vOCollidingNames.size());
+	for(int nName = 0; nName < nNames; nName++)
+	{
+		if(m_vOCollidingNames[nName] == a_sName)
+			return true;
+	}
+	return false;
+}
+
 void BoundingBoxManager::CollisionResponse(void)
 {
 	for(int nBox = 0; nBox < m_nBoxes; nBox++)
 	{
-		if(CheckForNameInList(m_vBoundingBox[nBox]->GetInstanceName()))
-			m_vBoundingBox[nBox]->SetColor(MERED);
+		if(CheckForNameInAAList(m_vBoundingBox[nBox]->GetInstanceName()))
+		{
+			m_vBoundingBox[nBox]->SetAAColor(MEMAGENTA);
+		}
+		if(CheckForNameInOList(m_vBoundingBox[nBox]->GetInstanceName()))
+		{
+			m_vBoundingBox[nBox]->SetOColor(MERED);
+		}
 	}
+}
+
+void BoundingBoxManager::SATCollisionCheck(void)
+{
+	for(int nBox2 = 0; nBox2 < m_nBoxes; nBox2++)
+	{
+		for(int nBox1 = 0; nBox1 < m_nBoxes; nBox1++)
+		{
+			if(nBox1 != nBox2)
+			{
+				if(SATCalculations(nBox1, nBox2))
+				{
+					m_vOCollidingNames.push_back(m_vBoundingBox[nBox1]->GetInstanceName());
+					m_vOCollidingNames.push_back(m_vBoundingBox[nBox2]->GetInstanceName());
+				}
+			}			
+		}		
+	}
+}
+
+bool BoundingBoxManager::SATCalculations(int nBox1, int nBox2)
+{
+	std::vector<vector3> vAxes1 = m_vBoundingBox[nBox1]->GetOAxes();
+	std::vector<vector3> vAxes2 = m_vBoundingBox[nBox2]->GetOAxes();
+
+	std::vector<vector3> vVertices1 = m_vBoundingBox[nBox1]->GetOVertices();
+	std::vector<vector3> vVertices2 = m_vBoundingBox[nBox2]->GetOVertices();
+
+
+	for(int nAxis = 0; nAxis < vAxes1.size();nAxis++)
+	{
+		vector2 v2Proj1 = vector2(INT_MAX, -INT_MAX);
+		vector2 v2Proj2 = vector2(INT_MAX, -INT_MAX);
+
+		vector3 v3Axis = vAxes1[nAxis];
+		for(int nVertex = 0; nVertex < vVertices1.size();nVertex++)
+		{
+			float fDot = glm::dot(v3Axis, vVertices1[nVertex]);
+
+			if(fDot < v2Proj1.x)
+				v2Proj1.x = fDot;
+			
+
+			if(fDot > v2Proj1.y)
+				v2Proj1.y = fDot;
+		}
+
+		for(int nVertex = 0; nVertex < vVertices2.size();nVertex++)
+		{
+			float fDot = glm::dot(v3Axis, vVertices2[nVertex]);
+
+			if(fDot < v2Proj2.x)
+				v2Proj2.x = fDot;
+
+			if(fDot > v2Proj2.y)
+				v2Proj2.y = fDot;
+		}
+
+		if(v2Proj1.x > v2Proj2.y)
+			return false;
+
+	}
+
+	for(int nAxis = 0; nAxis < vAxes2.size();nAxis++)
+	{
+
+		vector2 v2Proj1 = vector2(INT_MAX, -INT_MAX);
+		vector2 v2Proj2 = vector2(INT_MAX, -INT_MAX);
+
+		vector3 v3Axis = vAxes2[nAxis];
+
+		for(int nVertex = 0; nVertex < vVertices1.size();nVertex++)
+		{
+			float fDot = glm::dot(v3Axis,vVertices1[nVertex]);
+			if(fDot < v2Proj1.x)
+				v2Proj1.x = fDot;
+
+			if(fDot > v2Proj1.y)
+				v2Proj1.y = fDot;
+		}
+		for(int nVertex = 0; nVertex < vVertices2.size();nVertex++)
+		{
+			float fDot = glm::dot(v3Axis, vVertices2[nVertex]);
+			if(fDot < v2Proj2.x)
+				v2Proj2.x = fDot;
+
+			if(fDot > v2Proj2.y)
+				v2Proj2.y = fDot;
+		}
+
+		if(v2Proj1.x > v2Proj2.y)
+			return false;
+	}
+
+	return true;
 }
