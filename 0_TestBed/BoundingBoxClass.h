85558fcadd7923a1bdf514890a68da8a7de50124
Charnock <charnockz@yahoo.com>
2014-11-09 21:35:33 -0500

    Update BoundingBoxClass.h

--- 0_TestBed/BoundingBoxClass.h
+++ 0_TestBed/BoundingBoxClass.h
@@ -12,14 +12,27 @@ using namespace MyEngine;
 class BoundingBoxClass 
 {
 	String m_sInstance;//The name of the instance related to this Box
-	bool m_bVisible;//Flag for rendering or not
-	float m_fWidth;//Width of the Box
-	float m_fHeight;//Height of the Box
-	float m_fDepth;//Depth of the box
-	vector3 m_v3Centroid;//Centroid of the Box
-	vector3 m_v3Color;//Color of the Box
-	matrix4 m_mModelToWorld;//Model matrix of the Box
-	PrimitiveWireClass* m_pMesh;//Box Mesh
+	bool m_bAAVisible;//Flag for rendering the AA Box
+	bool m_bOVisible;//Flag for rendering the O Box
+	float m_fAAWidth;//Width of the AA Box
+	float m_fAAHeight;//Height of the AA Box
+	float m_fAADepth;//Depth of the AA box
+	float m_fOWidth;//Width of the O Box
+	float m_fOHeight;//Height of the O Box
+	float m_fODepth;//Depth of the O box
+	vector3 m_v3AAMaximum;
+	vector3 m_v3AAMinimum;
+	vector3 m_v3OMaximum;
+	vector3 m_v3OMinimum;
+	vector3 m_v3AACentroid;//Centroid of the AA Box
+	vector3 m_v3OCentroid;//Centroid of the O Box
+	vector3 m_v3AAColor;//Color of the AA Box
+	vector3 m_v3OColor;//Color of the O Box
+	std::vector<vector3> m_vOVertices;
+	std::vector<vector3> m_vOAxes;
+	matrix4 m_mModelToWorld;//Model matrix 
+	PrimitiveWireClass* m_pAAMesh;//AA Box Mesh
+	PrimitiveWireClass* m_pOMesh;//O Box Mesh
 	ModelManagerClass* m_pModelMngr;//ModelManager instance
 
 public:
@@ -33,14 +46,19 @@ public:
 	~BoundingBoxClass(void);
 
 	/* Gets the visibility of the Box (whether or not it is going to be drawn in render) */
-	bool GetVisible(void);
+	bool GetAAVisible(void);
+	bool GetOVisible(void);
 	/*Sets the visibility of the Box (whether or not it is going to be drawn in render)
 	Args:
 		bool a_bVisible -> true for draw false for not draw*/
-	void SetVisible(bool a_bVisible);
+	void SetAAVisible(bool a_bVisible);
+	void SetOVisible(bool a_bVisible);
 	/* Property:
 	GetVisible() or GetVisible()*/
-	__declspec(property(get = GetVisible, put = SetVisible)) bool Visible;
+	__declspec(property(get = GetAAVisible, put = SetAAVisible)) bool AAVisible;
+	__declspec(property(get = GetOVisible, put = SetOVisible)) bool OVisible;
+
+
 
 	/* Gets the name of the model associated with this bounding Box from model manager */
 	String GetInstanceName(void);
@@ -49,16 +67,33 @@ public:
 	__declspec(property(get = GetInstanceName)) String InstanceName;
 
 	/* Gets the centroid the bounding Box */
-	vector3 GetCentroid(void);
+	vector3 GetAACentroid(void);
+	vector3 GetOCentroid(void);
 	/* Property:
 	GetCentroid()*/
-	__declspec(property(get = GetCentroid)) vector3 Centroid;
+	__declspec(property(get = GetAACentroid)) vector3 AACentroid;
+	__declspec(property(get = GetOCentroid)) vector3 OCentroid;
 
 	/* Get the radius of the bounding Box */
 	float GetRadius(void);
 	/* Property:
 	GetRadius()	*/
 	__declspec(property(get = GetRadius)) float Radius;
+
+	vector3 GetAAMaximum(void);
+	vector3 GetAAMinimum(void);
+	vector3 GetOMaximum(void);
+	vector3 GetOMinimum(void);
+	std::vector<vector3> GetOAxes(void);
+	std::vector<vector3> GetOVertices(void);
+
+	__declspec(property(get = GetAAMaximum)) vector3 AAMaximum;
+	__declspec(property(get = GetAAMinimum)) vector3 AAMinimum;
+	__declspec(property(get = GetOMaximum)) vector3 OMaximum;
+	__declspec(property(get = GetOMinimum)) vector3 OMinimum;
+	__declspec(property(get = GetOAxes)) std::vector<vector3> OAxes;
+	__declspec(property(get = GetOVertices)) std::vector<vector3> OVertices;
+	
 
 	/* Gets the "ModelToWorld" matrix associated with the bounding Box */
 	matrix4 GetModelMatrix(void);
@@ -69,18 +104,22 @@ public:
 	__declspec(property(get = GetModelMatrix, put = SetModelMatrix)) matrix4 ModelMatrix;
 
 	/* Gets the color vector of this bounding Box (the default color in which is going to be rendered) */
-	vector3 GetColor(void);
+	vector3 GetAAColor(void);
+	vector3 GetOColor(void);
 	/* Sets the color vector of this bounding Box (the default color in which is going to be rendered) */
-	void SetColor(vector3 a_v3Color);
+	void SetAAColor(vector3 a_v3Color);
+	void SetOColor(vector3 a_v3Color);
 	/* Property:
 	GetColor() or SetColor() */
-	__declspec(property(get = GetColor, put = SetColor)) vector3 Color;
+	__declspec(property(get = GetAAColor, put = SetAAColor)) vector3 AAColor;
+	__declspec(property(get = GetOColor, put = SetOColor)) vector3 OColor;
 
 	/* Renders the bounding Box
 		Args:
 			a_vColor -> determinate the color of the Box to be rendered, if MEDEFAULT
 			it will render the shape in the constructed color (white) */
-	void Render( vector3 a_vColor = MEDEFAULT);
+	void RenderAA( vector3 a_vColor = MEDEFAULT);
+	void RenderO( vector3 a_vColor = MEDEFAULT);
 
 private:
 	/* Released Memory and objects allocated. */
@@ -88,7 +127,12 @@ private:
 	/* Calculates the Box from the instance
 	Args:
 		a_sInstance: The name of the instance for which the bounding Box is going to be calculated */
-	void CalculateBox(String a_sInstance);
+	void CalculateAABox(String a_sInstance);
+	void CalculateOBox(String a_sInstance);
+
+	// SAT helper methods
+	void CalculateOVertices(void);
+	void CalculateOAxes(void);
 };
 
 
